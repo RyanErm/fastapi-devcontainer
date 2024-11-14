5a65a7d3204816cb62eb21c25f07e8950900dd76
RyanErm <jph4dg@virginia.edu>
2024-11-14 13:10:27 -0500

    done

--- app/main.py
+++ app/main.py
@@ -3,35 +3,84 @@
 from fastapi import Request, FastAPI
 from typing import Optional
 from pydantic import BaseModel
-import pandas as pd
 import json
 import os
+import mysql.connector
+from mysql.connector import Error
 
-api = FastAPI()
 
-@api.get("/")  # zone apex
+app = FastAPI()
+
+from fastapi.middleware.cors import CORSMiddleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")  # zone apex
 def zone_apex():
     return {"Hello": "Hello API"}
 
-@api.get("/add/{a}/{b}")
+@app.get("/add/{a}/{b}")
 def add(a: int, b: int):
     return {"sum": a + b}
 
-@api.get("/customer/{idx}")
-def get_customer(idx: int):
-    # Read the data from the CSV file
+#@app.get("/customer/{idx}")
+#def get_customer(idx: int):
+    #get the data
     df = pd.read_csv("../customers.csv")
-    # Filter the data based on the index
+    #filter the data by index
     customer = df.iloc[idx]
+    #the output is the customer that we found, so we are making it a dictionary
     return customer.to_dict()
 
-@api.post("/get_body")
-async def get_body(request: Request):
-    return await request.json()
+#@api.get("/get_payload/{idx}")
+#async def get_payload(request: Request): #async and await are little markers telling the system that it is ok if the system takes a sec
+    #response = await request.json()
+    #geo = response.get("geo")
+    #url = "https://maps.google.com/?q={geo}".format(geo = geo)
 
-@api.post("/mapit")
-async def map_request(request: Request):
-    response = await request.json()
-    geo = response.get("geo")
-    url = "https://maps.google.com/?q={geo}".format(geo=geo)
-    return {"gmaps_url": url}
+    #return await request.json
+
+
+
+DBHOST = "ds2022.cqee4iwdcaph.us-east-1.rds.amazonaws.com"
+DBUSER = "admin"
+DBPASS = os.getenv('DBPASS')
+DB = "jph4dg"
+
+db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB)
+cur=db.cursor()
+
+
+@app.get('/genres')
+def get_genres():
+    query = "SELECT * FROM genres ORDER BY genreid;"
+    try:    
+        cur.execute(query)
+        headers=[x[0] for x in cur.description]
+        results = cur.fetchall()
+        json_data=[]
+        for result in results:
+            json_data.append(dict(zip(headers,result)))
+        return(json_data)
+    except Error as e:
+        return {"Error": "MySQL Error: " + str(e)}
+
+@app.get('/songs')
+def get_songs():
+    query = "SELECT * FROM songs;"
+    try:    
+        cur.execute(query)
+        headers=[x[0] for x in cur.description]
+        results = cur.fetchall()
+        return results
+        json_data=[]
+        for result in results:
+            json_data.append(dict(zip(headers,result)))
+        return(json_data)
+    except Error as e:
+        return {"Error": "MySQL Error: " + str(e)}
+
